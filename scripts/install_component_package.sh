fe49715a4e6c2773de03622c0e0c84aa442ab478
domire8 <71256590+domire8@users.noreply.github.com>
2022-03-17 14:11:14 +0100

    Improve install component package script (#3)
    * Improve install script
    
    * Help if no arguments provided
    
    * Add --force option
    
    * Require a destination dir to be set

--- scripts/install_component_package.sh
+++ scripts/install_component_package.sh
@@ -5,6 +5,7 @@
 # tilde extension is used to return absolute path for subsequent finds
 function install_dependencies() {
     COMPONENT_DIR="$(find ~+ -type d -name "$1" -print -quit)"
+    DESTINATION_DIR="$2"
 
     if [ -z "${COMPONENT_DIR}" ]; then
         echo "Component not found"
@@ -31,13 +32,77 @@ function install_dependencies() {
             bash "${SCRIPT}" || exit 1
         fi
     done
+
+    # eventually move the folder to the destination
+    if [ -n "${DESTINATION_DIR}" ]; then
+      echo "Moving component from ${COMPONENT_DIR} to ${DESTINATION_DIR}"
+      cp -r "${COMPONENT_DIR}" "${DESTINATION_DIR}"
+    fi
 }
 
-CURRENT_DIR=$(pwd)
-COMPONENT_PKG=$1
+HELP_MESSAGE="
+Usage: ./install_component_package.sh <pkg_names> -d <destination>
 
-if [ -n "${COMPONENT_PKG}" ]; then
-  cd "${CURRENT_DIR}" || exit 1
-  echo "Installing dependencies for component ${COMPONENT_PKG}"
-  install_dependencies "${COMPONENT_PKG}"
+Install a component package or a list of component packages to a certain directory, if
+provided a destination. This script will find and execute any 'install_dependencies.sh'
+scripts that contain the commands to install system dependencies as well as find and
+install any python packages provided in 'requirements.txt' files.
+
+Options:
+  pkg_names                         Name or list of names of the package(s)
+                                    that should be installed.
+
+  -d, --destination <destination>   The package(s) will be moved to the
+                                    specified directory (path can be absolute
+                                    or relative).
+
+  --force                           Force the component package and its dependencies
+                                    to be reinstalled if it already exists.
+
+  -h, --help                        Show this help message.
+"
+
+CURRENT_DIR=$(pwd)
+
+DESTINATION_DIR=""
+FORCE=0
+COMPONENT_LIST=()
+
+if [[ "$#" -eq 0 ]]; then
+  echo "${HELP_MESSAGE}"
+  exit 0
 fi
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    -d|--destination) DESTINATION_DIR="$2"; shift 2;;
+    --force) FORCE=1; shift 1;;
+    -h|--help) echo "${HELP_MESSAGE}"; exit 0;;
+    *) COMPONENT_LIST+=("$1"); shift 1;;
+  esac
+done
+
+# expand the path if it is relative
+if [ -n "${DESTINATION_DIR}" ]; then
+  if [[ ! "${DESTINATION_DIR}" = /* ]]; then
+    DESTINATION_DIR="${CURRENT_DIR}/${DESTINATION_DIR}"
+  fi
+  mkdir -p "${DESTINATION_DIR}"
+else
+  echo "No destination directory for the installation of the component package provided."
+  echo "${HELP_MESSAGE}" && exit 1
+fi
+
+for COMPONENT in "${COMPONENT_LIST[@]}"; do
+  if [ -n "${COMPONENT}" ]; then
+    cd "${CURRENT_DIR}" || exit 1
+    if [ "$(ls -A ${DESTINATION_DIR}/${COMPONENT})" ] && [ "${FORCE}" -eq 0 ]; then
+      echo "There already exists a component package named '${COMPONENT}'"
+      echo "in destination '${DESTINATION_DIR}'. Please use the '--force'"
+      echo "option if you wish to reinstall the component package and its dependencies." && exit 0
+    else
+      echo "Installing dependencies for component ${COMPONENT}"
+      install_dependencies "${COMPONENT}" "${DESTINATION_DIR}"
+    fi
+  fi
+done
