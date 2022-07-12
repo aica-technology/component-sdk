780c47b392864ff451ea2c968f5a08bd810c8f00
Enrico Eberhard <32450951+eeberhard@users.noreply.github.com>
2022-07-12 11:20:34 +0200

    Support refactored modulo components (#9)
    * Revise component package creation script
    
    * Make directory lookup more portable (readlink -m is not supported on Mac)
    
    * Rename variables and re-order code blocks for better readability
    
    * Exit with error if the package name is empty
    
    * Refactor to use modulo_components
    
    * Change base class of C++ and Python template components to
    be modulo_components::Components, and remove lifecycle methods
    
    * Rename PYComponent to PyComponent
    
    * Add LifecycleComponent templates
    
    * Add C++ and Python templates for the modulo_components
    LifecycleComponent derived class
    
    * Rename package in lifecycle component files
    
    * Fix package-to-image naming in template build-server script
    
    * Update README
    
    * Add missing modulo_components dependency
    
    * Supply fallback name for CPP components
    
    * Add on_deactivate_callback overrides
    
    * Add validate_parameter and on_execute_callback examples
    
    * Use hpp extension for C++  header files

--- source/template_component_package/template_component_package/py_lifecycle_component.py
+++ source/template_component_package/template_component_package/py_lifecycle_component.py
@@ -1,8 +1,7 @@
-from modulo_components.component import Component
+from modulo_components.lifecycle_component import LifecycleComponent
 import state_representation as sr
 
-
-class PyComponent(Component):
+class PyLifecycleComponent(LifecycleComponent):
     def __init__(self, node_name: str, *args, **kwargs):
         super().__init__(node_name, *args, **kwargs)
         # add parameters, inputs and outputs here
@@ -19,11 +18,18 @@ class PyComponent(Component):
         # return True if the new parameter value should be applied
         return True
 
-    def on_execute_callback(self) -> bool:
-        # If the component needs to do any post-construction behavior, invoke `self.execute()`
-        # at the end of the constructor, which will trigger this callback in a separate thread.
-        # This is only necessary when the behavior would otherwise block the constructor from completing
-        # in a timely manner, such as some time-intensive computation or waiting for an external trigger.
-
-        # return True if the execution was successful, False otherwise
+    def on_configure_callback(self) -> bool:
+        # configuration steps before activation
         return True
+
+    def on_activate_callback(self) -> bool:
+        # activation steps before running
+        return True
+
+    def on_deactivate_callback(self) -> bool:
+        # deactivation steps
+        return True
+
+    def on_step_callback(self):
+        # do something periodically
+        pass
