0f22ac75aa211be7c899b738ea3f5904dae1bf13
Dominic Reber <dominic@aica.tech>
2022-11-10 10:11:25 +0100

    Provide code to install dependencies in installation directory

--- source/template_component_package/install_dependencies.sh
+++ source/template_component_package/install_dependencies.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+if [ -z "${COMPONENTS_INSTALL_DIRECTORY}" ]; then
+  COMPONENTS_INSTALL_DIRECTORY=/tmp/components_installation
+fi
+mkdir -p "${COMPONENTS_INSTALL_DIRECTORY}" && cd "${COMPONENTS_INSTALL_DIRECTORY}" || exit 1
+
 # install any system dependencies using sudo apt get commands
+
+cd ${COMPONENTS_INSTALL_DIRECTORY} && rm -rf ./* || exit 1
